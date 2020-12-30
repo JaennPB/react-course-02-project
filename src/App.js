b0238d746c5b90c7e7f36ce07aa55ca30589da56
jaennpb <jaenn.bucio@gmail.com>
2020-12-30 11:55:06 -0600

    commit 1

--- src/App.js
+++ src/App.js
@@ -2,8 +2,24 @@ import React, { Component } from "react";
 import "./App.css";
 
 class App extends Component {
+  state = {
+    textLength: 0,
+  };
+
+  inputLength = (e) => {
+    const innerText = e.target.value;
+
+    console.log(innerText.length);
+    this.setState({ textLength: innerText.length });
+  };
+
   render() {
-    return <div className="App">hello</div>;
+    return (
+      <div className="App">
+        <input onChange={this.inputLength} />
+        <p>{this.state.textLength}</p>
+      </div>
+    );
   }
 }
 
